5bcc1a67421a2646422368f66440bee5bd7314d6
resteve <resteve@zikzakmedia.com>
2015-12-01 11:07:13 +0100

    Tests

--- tests/test_electronic_mail_filter.py
+++ tests/test_electronic_mail_filter.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the electronic_mail_filter module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ElectronicMailFilterTestCase(unittest.TestCase):
+class ElectronicMailFilterTestCase(ModuleTestCase):
     'Test Electronic Mail Filter module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('electronic_mail_filter')
-
-    def test0005views(self):
-        'Test views'
-        test_view('electronic_mail_filter')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'electronic_mail_filter'
 
 
 def suite():
